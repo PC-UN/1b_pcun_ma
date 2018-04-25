2dd73eb14ce1f78bfb6f35bfbc92d1f3fa2830d4
JuanitoAli <jcflorezv@unal.edu.co>
2018-04-24 22:35:57 -0500

    List with all points in CampaignsActivity...

--- app/src/main/java/com/pcun/b1/a1b_pcun_ma/CampaignActivity.java
+++ app/src/main/java/com/pcun/b1/a1b_pcun_ma/CampaignActivity.java
@@ -1,15 +1,16 @@
 package com.pcun.b1.a1b_pcun_ma;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 public class CampaignActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_campaign);
+
+        PointConnection pointConnection = new PointConnection();
+        pointConnection.allPoints(this);
     }
 }
--- app/src/main/java/com/pcun/b1/a1b_pcun_ma/PointConnection.java
+++ app/src/main/java/com/pcun/b1/a1b_pcun_ma/PointConnection.java
@@ -1,13 +1,18 @@
 package com.pcun.b1.a1b_pcun_ma;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.ApolloClient;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
+
+import java.util.ArrayList;
 
 import javax.annotation.Nonnull;
 
@@ -19,6 +24,9 @@ public class PointConnection {
     private OkHttpClient okHttpClient;
     private static final String URL = "http://35.196.104.239:3307/graphql";
 
+    private ArrayAdapter arrayAdapter;
+    private ListView listView;
+
     public PointConnection() {
         this.okHttpClient = new OkHttpClient.Builder().build();
         this.apolloClient = ApolloClient.builder()
@@ -27,21 +35,29 @@ public class PointConnection {
                 .build();
     }
 
-    public void allPoints(final AppCompatActivity context) {
+    public int allPoints(final Activity context) {
         apolloClient.query(
                 AllPoints.builder().build()
         ).enqueue(new ApolloCall.Callback<AllPoints.Data>() {
             @Override
             public void onResponse(@Nonnull final Response<AllPoints.Data> response) {
                 Log.d(TAG, "REQUEST SUCCEED!");
+                ArrayList<AllPoints.AllPoint> data = new ArrayList<>(response.data().allPoints());
+                final String[] names = new String[data.size()];
+                for(int i = 0; i < data.size(); i++)
+                    names[i] = data.get(i).name();
+
 
                 context.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        TextView txtResponse = (TextView) context.findViewById(R.id.textView);
-                        txtResponse.setText(response.data().toString());
+                        listView = (ListView) context.findViewById(android.R.id.list);
+                        arrayAdapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1, names);
+
+                        listView.setAdapter(arrayAdapter);
                     }
                 });
+
             }
 
             @Override
@@ -50,6 +66,7 @@ public class PointConnection {
                 Log.d(TAG, e.toString());
             }
         });
+        return 0;
     }
 
     public void pointById(int id, final AppCompatActivity context) {
